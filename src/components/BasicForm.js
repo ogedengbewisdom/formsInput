b92aa4c56b8b4a2cf1022ade1473b9a89fd0d124
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-28 23:40:31 +0100

    useState for two way binding

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,11 +1,46 @@
+import { useState } from "react";
+
 const BasicForm = (props) => {
+
+  const [firstName, setFirstName] = useState("")
+  const [firstNameTouched, setFirstNameTouched] = useState(null)
+
+  const firstNameIsValid = firstName.trim() !== ""
+  const firstNameIsInValid = !firstNameIsValid && firstNameTouched
+
+
+  const firstNameChangeHandler = (event) => {
+    setFirstName(event.target.value)
+  }
+
+  const firstNameBlurHandler = () => {
+    setFirstNameTouched(true)
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    console.log(firstName)
+    setFirstName("")
+    setFirstNameTouched(false)
+  }
+
+  const firstNameValidClass = firstNameIsInValid ? "form-control invalid" : "form-control"
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={firstNameValidClass}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input
+           type='text' 
+           id='name' 
+           onChange={firstNameChangeHandler}
+           onBlur={firstNameBlurHandler}
+           value={firstName} 
+           />
+           {firstNameIsInValid && <p className="error-text">Please enter your first name</p>}
         </div>
+        
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
           <input type='text' id='name' />
